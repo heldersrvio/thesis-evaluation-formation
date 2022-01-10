47d61231382d2f801c28fca2167560dc17685384
Helder Sérvio <heldersrvio@gmail.com>
2022-01-10 15:26:53 -0300

    Initial commit

---
+++ test/thesis-evaluation-formation.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ThesisEvaluationFormation = require('../lib/thesis-evaluation-formation-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/thesis-evaluation-formation-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ThesisEvaluationFormation.ThesisEvaluationFormationStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
